3c2f6ed4a88b3636cd32566bfc260ed86032505f
Eyeball Edward <edwardyeung.private@gmail.com>
2020-07-20 22:20:59 +1000

    game watcher

---
+++ modules/game-watcher.js
@@ -0,0 +1,27 @@
+let watcherSettings = require('../data/watch-games.json')
+
+module.exports = bot => {
+  let server = bot.guilds.cache.find(server => server.name === watcherSettings.server_name)
+  let channel = server.channels.cache.find(channel => channel.name === watcherSettings.channel_name)
+
+  bot.on('presenceUpdate', (oldPresence, newPresence) => {
+    let guild = newPresence.guild
+    let activities = newPresence.activities
+
+    let member = newPresence.member
+    if (member.user.id === bot.user.id) return
+
+    if (guild.id === server.id && activities.length) {
+      let activity = activities[0]
+      let {name} = activity
+
+      let activityType = activity.type.toString()
+
+      if (activityType === 'LISTENING') {
+        channel.send(`Attention! ${member} has just started listening to ${name}`)
+      } else {
+        channel.send(`Attention! ${member} has just started ${activityType.toLowerCase()} ${name}`)
+      }
+    }
+  })
+}
