ddf34933e72508d0a44fadddbb1269d33540ed58
Eyeball Edward <edwardyeung.private@gmail.com>
2020-08-06 10:24:45 +1000

    wtt command

---
+++ commands/wtt.js
@@ -0,0 +1,18 @@
+const timetables = require('../data/train-timetables')
+
+module.exports = {
+  name: 'wtt',
+  description: 'Gets a train timetable from the WTT',
+  exec: async (msg, args, bot) => {
+    let td = args[0]
+    if (!td) return msg.reply('You need to provide a TD: !wtt TD')
+
+    let matched = timetables.filter(timetable => timetable.runID === td)
+    matched.forEach(timetable => {
+      let text = timetable.tripTimings.map(stop => `${stop.locationName}: ${stop.departureTime || stop.arrivalTime}`).join('\n')
+      msg.reply(`Operates: ${timetable.days.join(', ')}
+Type: ${timetable.movementType}
+${text}`)
+    })
+  }
+}
