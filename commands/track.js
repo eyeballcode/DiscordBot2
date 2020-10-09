fa07dca30e93d32a0089f6c66b352b6693adb01c
Eyeball Edward <edwardyeung.private@gmail.com>
2020-10-09 23:50:40 +1100

    track

--- commands/track.js
+++ commands/track.js
@@ -2,26 +2,45 @@ const request = require('request-promise')
 
 module.exports = {
   name: 'track',
-  description: 'Tracks a bus or service',
+  description: 'Tracks a bus, train or service',
   exec: async (msg, args, bot) => {
     let mode = args[0], arg = args[1]
     if (mode) {
       let url
+      let type
       if (mode === 'service') {
         if (!arg) return msg.reply('You need to specify a service number')
         url = `https://vic.transportsg.me/bus/tracker/bot?service=${args[1]}`
+        type = 'bus'
       } else if (mode === 'bus') {
         if (!arg) return msg.reply('You need to specify a fleet number')
         url = `https://vic.transportsg.me/bus/tracker/bot?fleet=${args[1]}`
+        type = 'bus'
+      } else if (mode === 'train') {
+        if (!arg) return msg.reply('You need to specify a train number')
+        url = `https://vic.transportsg.me/metro/tracker/bot?consist=${args[1]}`
+        type = 'train'
+      } else if (mode === 'tdn') {
+        if (!arg) return msg.reply('You need to specify a TDN number')
+        url = `https://vic.transportsg.me/metro/tracker/bot?runID=${args[1]}`
+        type = 'train'
       } else return msg.reply('That doesn\'t look like a valid mode')
 
       let data = JSON.parse(await request(url))
 
-      let lines = data.map(trip => {
-        let line = `${trip.fleetNumber ? trip.fleetNumber + ': ' : ''}${trip.departureTime} ${trip.origin} - ${trip.destination}`
-        if (mode === 'bus') line += ` ${trip.routeNumber}`
-        return line
-      })
+      let lines
+      if (type === 'bus') {
+        lines = data.map(trip => {
+          let line = `${trip.fleetNumber ? trip.fleetNumber + ': ' : ''}${trip.departureTime} ${trip.origin} - ${trip.destination}`
+          if (mode === 'bus') line += ` ${trip.routeNumber}`
+          return line
+        })
+      } else if (type === 'train') {
+        lines = data.map(trip => {
+          let line = `${trip.runID}: ${trip.departureTime} ${trip.origin} - ${trip.destination}: ${trip.consist.join('-')}`
+          return line
+        })
+      }
 
       let chunks = []
       for (let i = 0; i < lines.length; i += 15) {
